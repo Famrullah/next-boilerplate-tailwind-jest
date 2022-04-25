b312e1e34b812a85bc2dbdb6c4be6685ad66c81f
faisal.amrullah <faisal.amrullah@klikdokter.com>
2022-04-26 00:31:56 +0700

    fixing fixing eslint jest

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import {render} from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import App from '../pages/index';
 
-const createWrapper = () =>{
-    const queryClient = new QueryClient({
-        defaultOptions: {
-          queries: {
-            retry: false,
-          },
-        },
-    });
-    return (
-        <QueryClientProvider client={queryClient}>
-             <App />
-        </QueryClientProvider>
-    );
+const createWrapper = () => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+  });
+  return (
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  );
 };
 
-describe('should render the app without crash',()=>{
-    const wrapper = createWrapper();
-    it('renders the home page',()=>{
-        render(wrapper);
-        expect(wrapper).toBeTruthy();
-    });
+describe('should render the app without crash', () => {
+  const wrapper = createWrapper();
+  it('renders the home page', () => {
+    render(wrapper);
+    expect(wrapper).toBeTruthy();
+  });
 });
