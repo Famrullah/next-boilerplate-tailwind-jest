18460f1539696e7569a4aff8765f8c4fa11691f8
Faisal <amrulahfaisal@gmail.com>
2022-05-01 08:40:04 +0700

    feat add home container

---
+++ containers/home/homeContainer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { IPosts } from '../../interfaces/postsInterfaces';
+
+const HomeContainer = (props:IPosts) => {
+  console.log(props);
+  return (
+    <>helloss</>
+  );
+};
+
+export default HomeContainer;
--- services/HomeApi.tsx
+++ services/HomeApi.tsx
@@ -1,7 +1,8 @@
 import axios from './api';
+import { IPosts } from '../interfaces/postsInterfaces';
 
-export const fetchSurat = async () => {
-  const req = await axios.get('/surat');
+export const fetchPosts = async (): Promise<IPosts[]> => {
+  const req = await axios.get('/posts');
   const res = await req.data;
   return res;
 };
