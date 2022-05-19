8af8e2c794ceff731269bac34d426dd8d9f8b2fc
Faisal Amrullah <faisal.amrullah@kitabisa.com>
2022-05-19 18:25:00 +0700

    feat add functional isEmpty checker

--- components/sidebar/parts/utils/sideNavData.tsx
+++ components/sidebar/parts/utils/sideNavData.tsx
@@ -1,9 +1,16 @@
 import { AiOutlineDashboard, AiTwotoneFileAdd } from 'react-icons/ai';
 
+interface IChildren {
+  text: string,
+  link: string,
+  icon: any,
+  children?: any[]
+}
 interface ISidenav {
   text: string,
   link: string,
-  icon: any
+  icon: any,
+  children?: IChildren[]
 }
 
 export const sideNavData: ISidenav[] = [
@@ -16,5 +23,29 @@ export const sideNavData: ISidenav[] = [
     text: 'Pengajuan',
     link: '/#',
     icon: AiTwotoneFileAdd,
+    children: [
+      {
+        text: 'Pinjaman',
+        link: '/#',
+        icon: AiOutlineDashboard,
+        children: [
+          {
+            text: 'KPR',
+            link: '/#',
+            icon: AiOutlineDashboard,
+          },
+          {
+            text: 'KTA',
+            link: '/#',
+            icon: AiOutlineDashboard,
+          },
+        ],
+      },
+      {
+        text: 'Pembukaan Rekening',
+        link: '/#',
+        icon: AiTwotoneFileAdd,
+      },
+    ],
   },
 ];
