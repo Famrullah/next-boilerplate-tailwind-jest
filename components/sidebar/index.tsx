bc7fdba3a51ebc917e457ef0e09ae760196f912d
Faisal Amrullah <faisal.amrullah@kitabisa.com>
2022-05-21 21:59:19 +0700

    style styling sidebar menu

--- components/sidebar/index.tsx
+++ components/sidebar/index.tsx
@@ -1,8 +1,6 @@
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
-import {
-  AiOutlineDashboard,
-} from 'react-icons/ai';
+import Image from 'next/image';
 import { Menus } from '../../utils/constants/menu';
 import MenusComponent from './parts/menu';
 import useStore from '../../store/useStore';
@@ -13,22 +11,19 @@ const Sidebar = () => {
     <div
       className={` ${
         isOpen ? 'w-60' : 'w-20 '
-      } bg-teal-700 h-screen p-5  pt-8 relative duration-300`}
+      } bg-red-900 h-screen p-5  pt-8 relative duration-300`}
     >
 
       <div className="flex gap-x-4 items-center">
-        <AiOutlineDashboard
-          className={`cursor-pointer duration-500 ${
+        <Image
+          src="/vercel.svg"
+          alt="my image"
+          width={150}
+          height={80}
+          className={`object-contain cursor-pointer duration-500 ${
             isOpen && 'rotate-[360deg] text-xl text-white'
           }`}
         />
-        <h1
-          className={`text-white origin-left font-bold text-xl duration-200 ${
-            !isOpen && 'scale-0'
-          }`}
-        >
-          Bleee
-        </h1>
       </div>
       <ul className="pt-6">
         <MenusComponent menu={Menus} open={isOpen} />
