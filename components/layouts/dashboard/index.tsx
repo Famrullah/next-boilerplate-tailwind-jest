66ea9343af371275bf0627283f2706643ef16eaf
Faisal Amrullah <faisal.amrullah@kitabisa.com>
2022-05-22 00:23:31 +0700

    feat add profil user ui

--- components/layouts/dashboard/index.tsx
+++ components/layouts/dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 import Sidebar from '../../sidebar';
 import useStore from '../../../store/useStore';
@@ -11,17 +12,23 @@ const AppLayout = ({ children }: any) => {
       <Sidebar />
       <div className="h-screen flex-1">
         <div className="relative">
-          <div className="fixed shadow-md w-full p-5 top">
+          <div className="fixed shadow-md w-full p-5 top relative flex flex flex-row">
             <div
-              className="cursor-pointer"
+              className="cursor-pointer basis-1/2 "
               aria-hidden
               onClick={() => setIsOpen()}
             >
               {!isOpen ? <AiOutlineClose /> : <AiOutlineMenu />}
             </div>
+            <div className="basis-1/3 absolute right-5 top-2 cursor-pointer">
+              <div className="relative inline-block">
+                <Image src="/user.jpeg" width={40} height={40} className="inline-block object-cover w-12 h-10 rounded-full" />
+                <span className="absolute bottom-1 right-0 inline-block w-3 h-3 bg-green-600 border-2 border-white rounded-full" />
+              </div>
+            </div>
           </div>
         </div>
-        <div className="p-5 mt-20">{children}</div>
+        <div className="p-5 mt-5">{children}</div>
       </div>
     </div>
   );
