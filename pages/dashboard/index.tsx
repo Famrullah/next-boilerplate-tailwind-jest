a044b6d65f943a0c08289a6044f865637eaee0de
Faisal Amrullah <faisal.amrullah@kitabisa.com>
2022-05-28 23:51:15 +0700

    feat add fetchpeople

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 import { getLayout } from '../../components/layouts/dashboard';
 import DashboardContainer from '../../containers/dashboard';
-// import { fetchPosts } from '../../services/HomeApi';
+import { fetchPosts } from '../../services/HomeApi';
 
-const Dashboard = () => (
-  <DashboardContainer />
-);
+const Dashboard = (props:any) => {
+  const req = useQuery(
+    'fetchPosts',
+    fetchPosts,
+    {
+      initialData: props,
+      staleTime: 0,
+    },
+  );
+
+  return (
+    <DashboardContainer {...req} />
+  );
+};
+
+export const getServerSideProps = async () => {
+  const data = await fetchPosts();
+  return {
+    props: {
+      query: data,
+    },
+  };
+};
 
 Dashboard.getLayout = getLayout;
 export default Dashboard;
